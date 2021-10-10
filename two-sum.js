54f80fd3e41b9d554d0f09b8bc0959427403b252
solenedel <solene98@student.ubc.ca>
2021-10-10 10:18:37 -0700

    complete two-sum

--- two-sum.js
+++ two-sum.js
@@ -10,6 +10,19 @@ You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 You can return the answer in any order. 
 
+EXAMPLE 1
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
+EXAMPLE 2
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+EXAMPLE 3
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
 */
 
 const twoSum = (nums, target) => {
